bd111a225939c9e517c50b220b4fd87115671cf0
Short Stack <41342213+rchlblns@users.noreply.github.com>
2018-11-08 20:20:03 -0600

    finished htmlRoute.js and connected to server.js

---
+++ routes/htmlRoute.js
@@ -0,0 +1,13 @@
+const path = require("path");
+
+module.exports = function(app){
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "./../public/index.html"));
+    });
+
+    app.get("/view/", function(req, res) {
+        // res.send("Hello!");
+        res.sendFile(path.join(__dirname, "./../public/view.html"));
+        
+    });
+};
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require("express");
+var htmlRoute = require("./routes/htmlRoute");
 
 var app = express();
+
+htmlRoute(app);
 
 var PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function(){
     
-})
+});
+
